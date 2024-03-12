f15fb522f2dd26ba48934d693f0035afc674c9d6
devmuraCode <muradkusherbaev@gmail.com>
2024-03-12 21:35:06 +0500

    added redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Root from "./Root.tsx";
-import { CartProvider } from "react-use-cart";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <CartProvider>
+
+  <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <Root />
     </QueryClientProvider>
-  </CartProvider>
+  </Provider>
 );
