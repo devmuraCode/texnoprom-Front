eec54d9b92fb9edc6447af7f805ea0c161cc9089
devmuraCode <muradkusherbaev@gmail.com>
2024-03-14 00:29:41 +0500

    shopping cart ready

--- src/modules/ProductItem/ui/ProductItem.tsx
+++ src/modules/ProductItem/ui/ProductItem.tsx
@@ -2,25 +2,32 @@ import { FC, useState } from "react";
 import { IProduct } from "../hooks/useProduct";
 import { useProductDetail } from "@/modules/ProductDetail/hooks/useProductDetail";
 import { NavLink } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "@/store/store";
 interface IProos {
   product: IProduct;
 }
 const ProductItem: FC<IProos> = (props) => {
+  const dispatch = useAppDispatch()
+  const {items} = useAppSelector(state => state.shoppingCart)
+  console.log(items);
+  
   const [productId, setProductId] = useState<string | null>(null);
   const { data: products } = useProductDetail({ productId });
+  
   return (
-    <NavLink
-      to={`/detail/${props.product.id}`}
-      onClick={() => setProductId(props.product.id)}
-    >
+    <div>
       <div className="w-full bg-white border-t border-x border-x-gray-200 duration-100  hover:shadow dark:bg-white dark:border-x-gray-200">
-        <a href="#">
+        <NavLink
+          to={`/detail/${props.product.id}`}
+          onClick={() => setProductId(props.product.id)}
+        >
           <img
             // className="rounded-t-lg"
             src={props.product.mainimg}
             alt="product image"
           />
-        </a>
+        </NavLink>
+
         <div className="px-5 pb-5">
           <a href="#">
             <p className="text-slate-600 font-semibold tracking-tight dark:text-grey-900 py-5">
@@ -38,13 +45,13 @@ const ProductItem: FC<IProos> = (props) => {
             </div>
           </div>
           <div className="flex items-center justify-between">
-            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button onClick={() => dispatch({type: 'shoppingCart/addItem', payload: props.product})} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Добавить в корзину
             </button>
           </div>
         </div>
       </div>
-    </NavLink>
+    </div>
   );
 };
 
