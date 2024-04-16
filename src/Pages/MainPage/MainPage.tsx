a5508ca4c2176ab22142fa2f01084bd0d48263b3
devmuraCode <muradkusherbaev@gmail.com>
2024-04-16 17:55:13 +0500

    asd

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -3,14 +3,10 @@ import Brand from "@/components/Brands/ui/Brand";
 import News from "@/components/News/ui/News";
 import Slider from "@/modules/Carousel/ui/Slider";
 import CollectionsCard from "@/modules/CollectionsCard/ui/CollectionsCard";
-import LoginModal from "@/modules/Modals/LoginModal";
-import RegisterModal from "@/modules/Modals/RegisterModal";
 
 const MainPage = () => {
   return (
     <div>
-      <RegisterModal />
-      <LoginModal/>
       <Slider />
       <CollectionsCard />
       <News />
