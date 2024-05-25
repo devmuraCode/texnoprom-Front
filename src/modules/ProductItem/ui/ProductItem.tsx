9f9d7f00a6472fe29199dfa986654166b089e378
Nurmuxammad <108805831+lilnurik@users.noreply.github.com>
2024-05-25 23:05:44 +0500

    updated product card and footer
    I have updated the product card to ensure that all photos are the same size, regardless of the device they are viewed on. I have also added functionality to the footer, removing unnecessary blocks and adding a brand transfer option. To achieve this, I used the use Effect hook.
    I apologize for using this method instead of hooks, as I did not fully understand the code structure at the time. However, together with my senior developer, we will work to fix this in the future.

--- src/modules/ProductItem/ui/ProductItem.tsx
+++ src/modules/ProductItem/ui/ProductItem.tsx
@@ -1,60 +1,64 @@
 import { FC, useState } from "react";
 import { IProduct } from "../hooks/useProduct";
 import { useProductDetail } from "@/modules/ProductDetail/hooks/useProductDetail";
-import { NavLink,  } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "@/store/store";
 import { addToCart } from "@/features/ShoppingSlice/CartSlice";
-
 import cls from "./ProductItem.module.scss";
+
 interface IProos {
   product: IProduct;
 }
+
 const ProductItem: FC<IProos> = (props) => {
-  const dispatch = useAppDispatch()
-  const {cartItems} = useAppSelector(state => state.cart)
+  const dispatch = useAppDispatch();
+  const { cartItems } = useAppSelector((state) => state.cart);
   const [productId, setProductId] = useState<string | null>(null);
   // @ts-ignore
   const { data: products } = useProductDetail({ productId });
-  
+
   const handleAddToCart = (product: any) => {
     dispatch(addToCart(product));
   };
 
   return (
-    <div>
-      <div className="w-full bg-white border-t border-x border-x-gray-200 duration-100  hover:shadow dark:bg-white dark:border-x-gray-200">
-        <NavLink
-          to={`/detail/${props.product.id}`}
-          onClick={() => setProductId(props.product.id)}
-        >
+    <div className="w-full bg-white border-t border-x border-x-gray-200 duration-100 hover:shadow dark:bg-white dark:border-x-gray-200">
+      <NavLink
+        to={`/detail/${props.product.id}`}
+        onClick={() => setProductId(props.product.id)}
+      >
+        <div className={cls.imageContainer}>
           <img
-            className="w-full"
+            className={cls.image}
             src={props.product.mainimg}
             alt="product image"
           />
-        </NavLink>
+        </div>
+      </NavLink>
 
-        <div className="px-5 pb-5">
-          <a href="#">
-            <p className="text-slate-600 font-normal tracking-tight dark:text-grey-900 py-5">
-              {props.product.title}
-            </p>
-          </a>
-          <div className="column items-center justify-between ">
-            <span className="text-lg font-bold text-gray-900 dark:text-black">
-              ${props.product.price}
-            </span>
+      <div className="px-5 pb-5">
+        <a href="#">
+          <p className="text-slate-600 font-normal tracking-tight dark:text-grey-900 py-5">
+            {props.product.title}
+          </p>
+        </a>
+        <div className="column items-center justify-between">
+          <span className="text-lg font-bold text-gray-900 dark:text-black">
+            ${props.product.price}
+          </span>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:bg-red-600 font-medium rounded-lg text-sm px-2 mb-5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800">
+            Цена в рассрочку
           </div>
-          <div className="flex items-center justify-between">
-            <div className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:bg-red-600 font-medium rounded-lg text-sm px-2 mb-5 text-center dark:bg-red-600 dark:hover:bbg-red-700 dark:focus:ring-blue-800">
-              Цена в рассрочку
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <button onClick={() => handleAddToCart(props.product)} className={cls.button}>
-              Добавить в корзину
-            </button>
-          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            onClick={() => handleAddToCart(props.product)}
+            className={cls.button}
+          >
+            Добавить в корзину
+          </button>
         </div>
       </div>
     </div>
