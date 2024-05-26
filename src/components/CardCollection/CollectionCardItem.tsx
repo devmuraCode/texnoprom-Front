d25c6077db1628d68602d09d0a2c6a6c8f40a5c6
Nurmuxammad <108805831+lilnurik@users.noreply.github.com>
2024-05-26 21:34:23 +0500

    fix-main-cards

--- src/components/CardCollection/CollectionCardItem.tsx
+++ src/components/CardCollection/CollectionCardItem.tsx
@@ -1,5 +1,6 @@
 import { ICollection } from "@/modules/CollectionsCard/hooks/useCollectionsCard";
 import { FC } from "react";
+import cls from "./CardCollection.module.scss";
 
 interface IProps {
     collection: ICollection;
@@ -9,11 +10,14 @@ const CollectionCardItem: FC<IProps> = (props) => {
 
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
+      <div className={cls.imageContainerCollection}>
       <img
         className="object-cover w-full h-48"
         src={collection.img}
         alt="Flower and sky"
+        id={cls.imageCollection}
       />
+      </div>
 
       <div className="absolute top-0 left-0 px-6 py-4">
         <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">
