600c27414ca14780334517092e2320542b620426
Nurmuxammad <108805831+lilnurik@users.noreply.github.com>
2024-05-26 20:44:20 +0500

    fix-product-detail

--- src/modules/ProductDetail/ProductDetail.tsx
+++ src/modules/ProductDetail/ProductDetail.tsx
@@ -1,36 +1,34 @@
 import { useParams } from "react-router-dom";
 import { useProductDetail } from "./hooks/useProductDetail";
 import Container from "@/components/Container/Container";
-import ProductItem from "../ProductItem";
+import ProductDetails from "../ProductDetails/ui/ProductDetails";
 import { useAppDispatch } from "@/store/store";
 import { addToCart } from "@/features/ShoppingSlice/CartSlice";
 
 const ProductDetail = () => {
-    const { productId } = useParams<{ productId: string }>();
-    const dispatch = useAppDispatch();
+  const { productId } = useParams<{ productId: string }>();
+  const dispatch = useAppDispatch();
 
-    // @ts-ignore
-    const { data: product, isLoading, isError } = useProductDetail({ productId });
+  // @ts-ignore
+  const { data: product, isLoading, isError } = useProductDetail({ productId });
 
-    const handleAddToCart = (product: any) => {
-        dispatch(addToCart(product));
-    };
+  const handleAddToCart = (product: any) => {
+    dispatch(addToCart(product));
+  };
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    if (isError || !product) {
-        return <div>Error loading product details.</div>;
-    }
-    
-    return (  
-        <Container>
-            <div>
-                <ProductItem product={product} onAddToCart={() => handleAddToCart(product)} />
-            </div>
-        </Container>
-    );
+  if (isError || !product) {
+    return <div>Error loading product details.</div>;
+  }
+
+  return (
+    <Container>
+      <ProductDetails product={product} onAddToCart={handleAddToCart} />
+    </Container>
+  );
 };
 
 export default ProductDetail;
---
+++ src/modules/ProductDetails/ui/ProductDetails.tsx
@@ -0,0 +1,60 @@
+import { FC } from "react";
+import { IProduct } from "../../ProductItem/hooks/useProduct";
+import cls from "./ProductDetails.module.scss";
+
+interface ProductDetailsProps {
+  product: IProduct;
+  onAddToCart: (product: IProduct) => void;
+}
+
+const ProductDetails: FC<ProductDetailsProps> = ({ product, onAddToCart }) => {
+  return (
+    <div className={cls.productDetailPage}>
+      <div className={cls.breadcrumbs}>
+        <a href="/">Главная</a> / <a href="/products">Телефоны, гаджеты, аксессуары</a> / {product.title}
+      </div>
+      <div className={cls.productDetailContainer}>
+        <div className={cls.imageContainer}>
+          <img className={cls.mainImage} src={product.mainimg} alt={product.title} />
+          <div className={cls.thumbnailContainer}>
+            {product.images?.map((img, index) => (
+              <img key={index} className={cls.thumbnail} src={img} alt={`${product.title} thumbnail ${index}`} />
+            ))}
+          </div>
+        </div>
+        <div className={cls.infoContainer}>
+          <h1 className={cls.title}>{product.title}</h1>
+          
+          <div className={cls.priceContainer}>
+            <span className={cls.currentPrice}>{product.price} UZS</span>
+            <span className={cls.monthlyPrice}>363,000 сум/мес.</span>
+            <span className={cls.oldPrice}>3,599,000 UZS</span>
+            <span className={cls.discount}>Вы экономите: 600,000 UZS</span>
+          </div>
+          <div className={cls.availability}>
+            Наличие в магазинах: <span className={cls.store}>Ташкент</span> В наличии в 1 магазине
+          </div>
+          <div className={cls.buttons}>
+            <button className={cls.addToCartButton} onClick={() => onAddToCart(product)}>В КОРЗИНУ</button>
+            <button className={cls.installmentButton}>В РАССРОЧКУ</button>
+          </div>
+          <div className={cls.contact}>
+            Продавец: <span className={cls.seller}>ELMAKON</span> <a href="tel:+998712031203">+998-71-203-1203</a>
+          </div>
+        </div>
+      </div>
+      <div className={cls.tabs}>
+        <div className={cls.tab}>Описание</div>
+        <div className={cls.tab}>Характеристики</div>
+        <div className={cls.tab}>Теги</div>
+        <div className={cls.tab}>Отзывы</div>
+        <div className={cls.tab}>Наличие в магазинах</div>
+      </div>
+      <div className={cls.description}>
+        {product.description}
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
