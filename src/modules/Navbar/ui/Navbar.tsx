5b6aef2abaac4ed67070012c827aece4e469e798
devmuraCode <muradkusherbaev@gmail.com>
2024-06-11 19:08:59 +0500

    tex

--- src/modules/Navbar/ui/Navbar.tsx
+++ src/modules/Navbar/ui/Navbar.tsx
@@ -1,21 +1,43 @@
 import { Link, NavLink } from "react-router-dom";
 import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
 import { Input, Space } from "antd";
-
 import Dropdown from "@/components/Dropdawn/Dropdown";
 import Category from "./Category";
 import logo from "@/assets/logo1.svg";
 import cls from "./Navbar.module.scss";
 import useRegisterModal from "@/modules/Modals/hooks/useRegisterModal";
+import { useState } from "react";
+import { useAllProducts } from "@/modules/ProductItem/hooks/useAllProducts";
+import { useProductDetail } from "@/modules/ProductDetail/hooks/useProductDetail";
 
 const Navbar = () => {
   const registerModal = useRegisterModal();
+
+  const { data: products = [] } = useAllProducts();
+
+  const [productId, setProductId] = useState<string | null>(null);
+  // @ts-ignore
+  const { data: productsDetail } = useProductDetail({ productId });
+
   const token = localStorage.getItem("token");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.reload();
   };
+
+  const handleProductClick = () => {
+    setSearchTerm("");
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className={cls.wrapper}>
@@ -40,30 +62,65 @@ const Navbar = () => {
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse lg:pb-0">
             <div className="font-bold items-center py-2 px-4 border-0 bg-inherit text-white flex gap-5 text-xl">
               {!token ? (
-                <div className={cls.exit} onClick={() => registerModal.onOpen()}>
+                <div
+                  className={cls.exit}
+                  onClick={() => registerModal.onOpen()}
+                >
                   <div className="flex gap-2">
                     <UserOutlined />
-                    <h1 className="text-white text-base lg:block hidden">Войти</h1>
+                    <h1 className="text-white text-base lg:block hidden">
+                      Войти
+                    </h1>
                   </div>
                 </div>
               ) : (
                 <div className={cls.exit} onClick={handleLogout}>
                   <div className="flex gap-2">
                     <UserOutlined />
-                    <h1 className="text-white text-base lg:block hidden">Выйти</h1>
+                    <h1 className="text-white text-base lg:block hidden">
+                      Выйти
+                    </h1>
                   </div>
                 </div>
               )}
               <NavLink to="/cart" className={cls.cart}>
                 <ShoppingCartOutlined />
-                <h1 className="text-white text-base lg:block hidden">Корзина</h1>
+                <h1 className="text-white text-base lg:block hidden">
+                  Корзина
+                </h1>
               </NavLink>
             </div>
           </div>
           <div className="md:flex w-full md:w-auto md:order-1">
-            <Input className={cls.input} placeholder="Search product" />
+            <Input
+              className={cls.input}
+              placeholder="Search product"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
           </div>
         </div>
+        {searchTerm && (
+          <div className="max-w-screen-xl mx-auto p-4">
+            <ul className={cls.searchResults}>
+              {filteredProducts.map((product) => (
+                <div className="flex gap-3 items-stretch">
+                  <Link
+                    to={`/detail/${product.id}`}
+                    onClick={() => {
+                      setProductId(product.id);
+                      handleProductClick();
+                    }}
+                    key={product.id}
+                    className={cls.searchResultItem}
+                  >
+                    {product.title} - ${product.price}
+                  </Link>
+                </div>
+              ))}
+            </ul>
+          </div>
+        )}
       </nav>
     </div>
   );
